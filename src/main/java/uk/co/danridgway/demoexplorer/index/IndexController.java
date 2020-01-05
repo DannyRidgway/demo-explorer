ed31d2530512260322ac3be5aff051c4967a3483
Daniel Ridgway <git@danridgway.co.uk>
2020-01-05 17:01:03 +0000

    Including Page/Component and Library CSS and JS

--- src/main/java/uk/co/danridgway/demoexplorer/index/IndexController.java
+++ src/main/java/uk/co/danridgway/demoexplorer/index/IndexController.java
@@ -13,12 +13,16 @@ public class IndexController {
     public String index(Model model) {
         ModelUtils modelutils = new ModelUtils();
         modelutils.setPage("index");
+        modelutils.setComponents(new String[]{"index"});
         modelutils.setModel(model);
         model = modelutils.setupModel();
+
         String test = "World";
         model.addAttribute("test", test);
 
         // TODO Idea is to have multiple components per page e.g. demodetails/demodetails, demodetails/scoreboardComponent
+        // TODO Pass back the main page which will then include all components.
+        // TODO This controller will need to get all the data needed for all components on first load.
         return "index/index";
     }
 
--- src/main/java/uk/co/danridgway/demoexplorer/utils/ModelUtils.java
+++ src/main/java/uk/co/danridgway/demoexplorer/utils/ModelUtils.java
@@ -5,15 +5,18 @@ import org.springframework.ui.Model;
 public class ModelUtils {
     private String page;
     private Model model;
+    private String[] components;
 
     public ModelUtils() {}
 
     public void setModel(Model modelObj){
         model = modelObj;
     }
-
     public void setPage(String pageID){
         page = pageID;
+    }
+    public void setComponents(String[] componentList) {
+        components = componentList;
     }
 
     public Model setupModel(){
@@ -21,8 +24,9 @@ public class ModelUtils {
         MessageUtils messageutils = new MessageUtils(page);
         //Object messages = messageutils.getMessages();
         //model.addAttribute("messages", messages);
+        model.addAttribute("page", page);
+        model.addAttribute("components", components);
 
         return model;
     }
-
 }
